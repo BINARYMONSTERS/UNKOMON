6228680874327e62ffbf5767f2bb0bf70541f42b
レミコン <46648011+remikon@users.noreply.github.com>
2024-10-01 21:23:41 +0900

    Add ServerSetting

---
+++ Unkomon_Server/src/merkle-tree.js
@@ -0,0 +1,67 @@
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { keypairIdentity, generateSigner } from "@metaplex-foundation/umi";
+import { createTree } from "@metaplex-foundation/mpl-bubblegum";
+import { getConfig } from "./common.js";
+import { loadData, saveData } from "./storage.js";
+
+const MERKLE_TREE_PUBLIC_KEY = "merkleTreePublicKey";
+const MERKLE_TREE_SECRET_KEY = "merkleTreeSecretKey";
+
+// Get Merkle tree
+// @return {
+//   publicKey: string,
+//   secretKey: number[],
+// } | null
+export const getMerkeTree = async () => {
+  const publicKey = await loadData(MERKLE_TREE_PUBLIC_KEY);
+  const secretKey = await loadData(MERKLE_TREE_SECRET_KEY);
+  if (!publicKey || !secretKey) {
+    return null;
+  }
+  return {
+    publicKey,
+    secretKey,
+  };
+};
+
+// Create a new Merkle tree
+// @param wallet: {
+//   publicKey: string,
+//   secretKey: number[],
+// } - Payer wallet information
+// @return {
+//   publicKey: string,
+//   secretKey: string,
+// }
+export const createMerkleTree = async (wallet) => {
+  const config = getConfig();
+  const umi = createUmi(config.endpoint);
+
+  const secretKeyUInt8Array = new Uint8Array(wallet.secretKey);
+  const payerKeypair =
+    umi.eddsa.createKeypairFromSecretKey(secretKeyUInt8Array);
+  umi.use(keypairIdentity(payerKeypair));
+
+  const merkleTree = generateSigner(umi);
+
+  const builder = await createTree(umi, {
+    merkleTree,
+    // maxDepth: 3,
+    // maxBufferSize: 8,
+    maxDepth: 14,
+    maxBufferSize: 64,
+  });
+
+  await builder.sendAndConfirm(umi);
+
+  publicKey = merkleTree.publicKey.toString();
+  secretKey = Array.from(merkleTree.secretKey);
+
+  await saveData(MERKLE_TREE_PUBLIC_KEY, publicKey);
+  await saveData(MERKLE_TREE_SECRET_KEY, secretKey);
+
+  return {
+    publicKey,
+    secretKey,
+  };
+};
